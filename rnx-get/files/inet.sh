1bdb0ac178c7121a777ad8312457a6c867fb7851
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2021-06-12 08:55:47 +0530

    Create inet.sh

---
+++ rnx-get/files/inet.sh
@@ -0,0 +1,24 @@
+#!/data/data/com.awnto.rinix.io/files/usr/kbin/bash
+
+
+pdir=$HOME/.awnto
+pfol=rnx-d
+pfll=$pdir/$pfol
+
+mkdir -p $pfll
+
+cd $pdir
+
+cd $pfol/fil/rcv/var
+
+
+#cat $pfol/fil/rcv/welcome.txt
+
+
+rm work_inet
+#touch work_inet
+wget -O work_inet "https://awntodataserv.000webhostapp.com/eco.php?rdu=`cat $pdir/$pfol/im/vari/rdu`&rfl=`cat $pdir/$pfol/im/vari/rnx_rfl`&ver=`rnx version`"
+
+
+
+
